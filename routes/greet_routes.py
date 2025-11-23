4c656a3890937ec9f7f7ad681e79970cbd4efa34
agreem <agreemsaini50510@gmail.com>
2025-11-24 02:15:08 +0530

    Deleted

--- routes/greet_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-greet_bp = Blueprint("greet", __name__)
-
-@greet_bp.route("/greet")
-def greet():
-    return jsonify({"message": "Hello from Flask backend!"})
